6306b7a60b8b8f3a8b1e7970c4796a529126e16a
ungspiller <ssbergh@hotmail.no>
2022-01-26 23:16:33 +0100

    Minimal express server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/api/movies", (req, res) => {
+    res.json([
+        {title: "Hello"}
+    ])
+});
+
+const server = app.listen(3000, () => {
+    console.log("listening on http://localhost:" + server.address().port);
+});
