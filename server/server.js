7de4be9f62064976022789012249d980e36872ce
ungspiller <ssbergh@hotmail.no>
2022-01-27 17:19:10 +0100

    Moved /api/movies to express from React

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -16,10 +17,17 @@ const MOVIES = [
     }
 ];
 
+app.use(bodyParser.json());
 
 app.get("/api/movies", (req, res) => {
     res.json(MOVIES);
 });
+
+app.post("/api/movies", (req, res) => {
+    const {title, year, plot} = req.body;
+    MOVIES.push({title, year, plot});
+    res.sendStatus(200);
+})
 
 app.use(express.static(path.resolve("..", "client", "dist")));
 app.use((req, res) => {
@@ -27,6 +35,6 @@ app.use((req, res) => {
 });
 
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
     console.log("listening on http://localhost:" + server.address().port);
 });
